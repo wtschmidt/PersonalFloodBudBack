0a92380c9f400cda268bbe239d79a29eb95afeb5
Carin Chapman <Chapman.carin@gmail.com>
2019-10-04 17:04:06 -0500

    Make API call to Accuweather return the desired data only

--- server/APIhelpers.js
+++ server/APIhelpers.js
@@ -1,7 +1,27 @@
 const axios = require('axios');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
+
+const { ACCUWEATHER_APIKEY } = process.env;
 
 const getRainfall = () => {
-  axios.get(`http://dataservice.accuweather.com/currentconditions/v1/348585?apikey=${ACCUWEATHER_APIKEY}`)
-    .then((rain) => rain);
+  return axios.get(`http://dataservice.accuweather.com/currentconditions/v1/348585?apikey=${ACCUWEATHER_APIKEY}&details=true`)
+    .then((allWeather) => allWeather.data[0].PrecipitationSummary.Precipitation.Imperial.Value);
 };
+
+
+module.exports = {
+  getRainfall,
+};
+
+const getLastFiveResults = () => new Promise((resolve, reject) => {
+  connection.query(
+    'SELECT setup, punchline, cocktail FROM (SELECT * FROM jokesAndCocktails ORDER BY id DESC LIMIT 5) sub ORDER BY id ASC',
+    (err, topFive) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(topFive);
+      }
+    },
+  );
+});
