4214d023da153fa3b9366a501bf4c140c0a733aa
Carin Chapman <Chapman.carin@gmail.com>
2019-10-03 19:22:47 -0500

    Add an GET request for the test function insertUser inside dbindex.js. Clean up code.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { insertUser } = require('../database/dbindex');
 
 const PORT = process.env.PORT || 8080;
 
@@ -14,24 +15,10 @@ app.use(bodyParser.json());
 const angularStaticDir = path.join(__dirname, '../../flood/dist/flood');
 
 app.use(express.static(angularStaticDir));
-// app.use('/static', express.static(path.join(__dirname, '../../flood/dist')));
-// app.use('api/', express.static(path.join(__dirname, '../../flood/dist')));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
-// app.use(express.static('../../flood/dist'));
-// app.get('/api/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../flood/dist'));
-// });
 
 app.get('/route', (req, res) => {
   axios.get('https://api.openbrewerydb.org/breweries')
     .then((breweries) => {
-      // console.log(breweries);
       res.status(201).send(breweries.data);
     })
     .catch((err) => {
@@ -39,6 +26,19 @@ app.get('/route', (req, res) => {
       res.send(500);
     });
 });
+
+app.get('/addUser', (req, res) => {
+  insertUser()
+    .then((results) => {
+      console.log(results);
+      res.send(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send(500);
+    });
+});
+
 
 app.listen(PORT, () => {
   console.log('Floodbuddies be listening on: 8080');
