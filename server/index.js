80dbb4c3aa66bb52c9d9d5d5aeb9fb6d42409f26
Wesley <wtschmidt94@gmail.com>
2019-10-10 09:32:44 -0500

    created aeris and fixed createAddress()

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const AerisWeather = require('@aerisweather/javascript-sdk');
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
@@ -7,10 +8,11 @@ const { getRainfall, createAddress } = require('./APIhelpers');
 
 const PORT = process.env.PORT || 8080;
 
-
 const app = express();
 
 app.use(bodyParser.json());
+
+const aeris = new AerisWeather(process.env.AERIS_ID, process.env.AERIS_KEY);
 
 // Property of Pesto -> API_KEY: AIzaSyAbJOa8X-CeBSal5VFPQPT1Qkhd-XTnf0s
 // Property of Pesto -> Weather_KEY: yepxYOZ9rt28QeEGiMu0tW8K75qlkrHG
@@ -21,6 +23,33 @@ const angularStaticDir = path.join(__dirname, '../../Floods-thesis/dist/flood');
 app.use(express.static(angularStaticDir));
 
 let reportData;
+
+app.get('/radar', (req, res) => {
+  // aeris.api().endpoint('observations').place('new orleans,la').get()
+  //   .then((result) => {
+  //     const data = result.data.ob;
+  //     console.log(`The current weather is ${data.weatherPrimary.toLowerCase()} and ${data.tempF} degrees.`);
+  //   });
+  aeris.views()
+    .then((views) => {
+      // const map = new views.InteractiveMap(document.getElementById('map'), {
+      //   center: {
+      //     lat: 39.0,
+      //     lon: -95.5,
+      //   },
+      //   zoom: 4,
+      //   strategy: 'google',
+      //   accessToken: 'GOOGLE_KEY',
+      //   layers: 'radar,alerts',
+      //   timeline: {
+      //     from: -6 * 3600,
+      //     to: 0,
+      //   },
+      // });
+      // console.log(map);
+      res.json(views);
+    });
+});
 
 app.get('/route', (req, res) => {
   axios.get('https://api.openbrewerydb.org/breweries')
